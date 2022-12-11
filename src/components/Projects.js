091da623c904e57153636ca90c8a2e6603058cd4
AlexDruzina <alexdruzina@gmail.com>
2022-12-11 13:49:21 +0200

    add mui paper to projects and skill component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
 import {projects} from "../data";
-import "./style.css"
-import Button from "@mui/material/Button";
+import "./style.css";
 import ProjectInfo from "../layout/ProjectInfo";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
 
 
 export default function Projects() {
@@ -18,21 +20,25 @@ export default function Projects() {
     };
     return (
         <section className="projects">
+            <Paper occupation={3} style={{background:"#ffffff1a"}}>
+                {projects.map((project) => (
+                    <div style={{display: "flex", padding: "50px"}}
+                         key={project.image}>
+                        <Link
+                            style={{color: "white", textDecoration: "none", cursor: "pointer"}}
+                            onClick={() => {
+                                handleSelected(project)
+                            }}>
+                            <Typography variant="h5">
+                                {project.title}
+                            </Typography>
+                        </Link>
 
-            {projects.map((project) => (
-                <div style={{display: "flex"}}
-                     key={project.image}>
-                    <div>
-                        <Button onClick={() => {handleSelected(project)}}>
-                            {project.title}
-                        </Button>
                         <ProjectInfo project={selectedProject} handleClose={handleClose} open={open}/>
 
-
                     </div>
-
-                </div>
-            ))}
+                ))}
+            </Paper>
         </section>
     );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,21 +1,29 @@
 import React from "react";
 import "./style.css"
+import Paper from "@mui/material/Paper";
 
 export default function Skills() {
     return (
         <section className="skills">
-            <h2>Programming skills and technologies</h2>
-            <p><b>Python</b>: Flask, SQLAlchemy, NumPy, Pandas, and other ML libraries; familiar with PyTorch.</p>
-            <p><b>Javascript</b>: React, Web: HTML, CSS.</p>
-            <p>Some academic experience with <b>Java</b>, <b>C++</b>, <b>C#</b>.</p>
-            <br></br>
-            <br></br>
-            <h2>Education </h2>
-            <h2> 2018 - 2022 Technion, Haifa | B.Sc in Information Systems</h2>
-            <br></br>
-            <li> A B.Sc graduate of the 4-year program in the field of information systems, 84.5 GPA.</li>
-            <li> During my studies I’ve been awarded 3 times by the Dean of the faculty for my excellence in studies.</li>
-            <li>I’ve been mentoring first-year students in Python for nearly a year.</li>
+            <Paper occupation={3} style={{background: "#ffffff1a", padding: 50,color:"white"}}>
+
+                    <h2>Programming skills and technologies</h2>
+                    <p><b>Python</b>: Flask, SQLAlchemy, NumPy, Pandas, and other ML libraries; familiar with PyTorch.
+                    </p>
+                    <p><b>Javascript</b>: React, Web: HTML, CSS.</p>
+                    <p>Some academic experience with <b>Java</b>, <b>C++</b>, <b>C#</b>.</p>
+                    <br></br>
+                    <br></br>
+                    <h2>Education </h2>
+                    <h2> 2018 - 2022 Technion, Haifa | B.Sc in Information Systems</h2>
+                    <br></br>
+                    <li> A B.Sc graduate of the 4-year program in the field of information systems, 84.5 GPA.</li>
+                    <li> During my studies I’ve been awarded 3 times by the Dean of the faculty for my excellence in
+                        studies.
+                    </li>
+                    <li>I’ve been mentoring first-year students in Python for nearly a year.</li>
+
+            </Paper>
         </section>
     );
 }
