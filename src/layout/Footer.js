3bbf0b0812f950d71b023536d244645367cd3289
AlexDruzina <alexdruzina@gmail.com>
2022-12-11 15:38:14 +0200

    update Footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -2,14 +2,14 @@ import React from "react";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import IconButton from "@mui/material/IconButton";
-
+import "../icons/icons_style.css"
 const Footer = () => {
     return <footer>
-        <IconButton aria-label="linkedin"
+        <IconButton className="zoom" aria-label="linkedin"
                     onClick={() => window.open('https://www.linkedin.com/in/alex-druzina', '_blank')}>
             <LinkedInIcon style={{color: "white"}}/>
         </IconButton>
-        <IconButton style={{color: "white"}}
+        <IconButton className="zoom" style={{color: "white"}}
                     onClick={() => window.open('https://github.com/saashd', '_blank')}>
             <GitHubIcon/>
         </IconButton>
