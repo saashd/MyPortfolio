82758b4f2711518a069655c6cc528582e470a9d3
AlexDruzina <alexdruzina@gmail.com>
2022-12-11 14:00:24 +0200

    update data

--- src/data.js
+++ src/data.js
@@ -5,8 +5,8 @@ export const projects = [
         description:
             "A web application for advisors and students. Currently serving over 100 students and advisors each semester.\n" +
             "● Backend in Python (Flask, SQLAlchemy), Frontend in Javascript (React)," +
-            "Firebase for SSO and persisting key-value (Json) data, MySQL for " +
-            "persisting internal table-based data" +
+            "Firebase for SSO and persisting key-value (Json) data, MySQL for" +
+            "persisting internal table-based data.\n" +
             "● Best project award in 2021.",
         image: require('./icons/icon_cpms.jpg'),
         link: "https://cpms.iem.technion.ac.il/",
@@ -15,16 +15,26 @@ export const projects = [
         title: "Health Tracker App",
         subtitle: "IOT Project for Android",
         description:
-            "A mobile application for health traking. Used Arduino to track steps during a walk or a run together with route, and to track vital signs." +
-            "● Technologies: Java, Google APIs, Firebase " +
-            "● Demonstration video can be found in GitHub repo.",
+            "A mobile application for health traking. Used Arduino to track steps during a walk or a run together with route, and to track vital signs.\n" +
+            "● Technologies: Java, Google APIs, Firebase.\n" +
+            "● Demonstration video can be found in GitHub repo.\n",
         image: "",
         link: "https://github.com/saashd/IOT-Project",
     },
     {
+        title: "Meme Generator",
+        subtitle: "",
+        description:
+            "Web App for creating custom memes.\n" +
+            "● Technologies:  JS,React,Material UI and imgflip \n" +
+            "● Live Demo: stellular-jelly-eaf7b4.netlify.app ",
+        image: require('./icons/meme.jpg'),
+        link: "https://github.com/saashd/meme_generator",
+    },
+    {
         title: "ML Projects",
         subtitle: "",
-        description: "Project following “ControlVAE: Tuning, Analytical Properties, and Performance Analysis” paper",
+        description: "Project following “ControlVAE: Tuning, Analytical Properties, and Performance Analysis” paper \n",
         image: "",
         link: "https://github.com/saashd/controlVAE",
     },
