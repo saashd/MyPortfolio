d3c491032d58b14caa16edfcbe8e2a7243b13c83
AlexDruzina <alexdruzina@gmail.com>
2022-12-11 15:04:11 +0200

    update skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,28 +1,47 @@
 import React from "react";
 import "./style.css"
+import "../icons/icons_style.css"
 import Paper from "@mui/material/Paper";
+import {technologies} from "../data";
+import Grid from "@mui/material/Grid";
+import technion_logo from "../icons/technion.png"
 
 export default function Skills() {
     return (
         <section className="skills">
-            <Paper occupation={3} style={{background: "#ffffff1a", padding: 50,color:"white"}}>
-
-                    <h2>Programming skills and technologies</h2>
-                    <p><b>Python</b>: Flask, SQLAlchemy, NumPy, Pandas, and other ML libraries; familiar with PyTorch.
-                    </p>
-                    <p><b>Javascript</b>: React, Web: HTML, CSS.</p>
-                    <p>Some academic experience with <b>Java</b>, <b>C++</b>, <b>C#</b>.</p>
-                    <br></br>
-                    <br></br>
-                    <h2>Education </h2>
-                    <h2> 2018 - 2022 Technion, Haifa | B.Sc in Information Systems</h2>
-                    <br></br>
-                    <li> A B.Sc graduate of the 4-year program in the field of information systems, 84.5 GPA.</li>
+            <Paper align="center" occupation={3} style={{background: "#ffffff9c", padding: 50}}>
+                <h2>Technical Skills</h2>
+                <Grid container spacing={2}>
+                    {technologies.map(item => (
+                        <Grid item sx={12} key={item.url}>
+                            <img className="zoom"
+                                 src={item.url}
+                                 alt={item.name}
+                                 loading="lazy"
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+                <p> NumPy, Pandas, and other ML libraries; familiar with PyTorch.</p>
+                <p>Some academic experience with <b>Java</b>, <b>C++</b>, <b>C#</b>.</p>
+                <br></br>
+                <h2>Education</h2>
+                <div style={{display: "flex"}}>
+                    <img width="40px"
+                         height={"50px"}
+                         src={technion_logo}
+                         alt={"technion logo"}
+                         loading="lazy"
+                    />
+                    <h3>Technion, Haifa | B.Sc in Information Systems</h3>
+                </div>
+                <div style={{textAlign:"left"}}>
                     <li> During my studies I’ve been awarded 3 times by the Dean of the faculty for my excellence in
-                        studies.
-                    </li>
-                    <li>I’ve been mentoring first-year students in Python for nearly a year.</li>
+                    studies.
+                </li>
+                <li>I’ve been mentoring first-year students in Python for nearly a year.</li>
 
+                </div>
             </Paper>
         </section>
     );
