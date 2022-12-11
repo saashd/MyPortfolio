116806936157701d1e36957f45e5e1bcdf8bc22c
AlexDruzina <alexdruzina@gmail.com>
2022-12-11 15:36:08 +0200

    update About component

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,6 @@ export default function About() {
             </p>
             <p id='head3' className="header">
                 Looking for my first position as a Front End\Full Stuck Developer
-                I'm a well-organized person, problem solver with high attention to details.
             </p>
             <a className="button1 btn"
                href={cv} download="Alex Druzina - CV.pdf">Resume</a>
