9218857f87531d944cc87b50ca31ef1ba8e65bdf
AlexDruzina <alexdruzina@gmail.com>
2022-12-12 17:01:30 +0200

    implemented contact form using emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,75 +1,117 @@
+import {useForm} from 'react-hook-form';
 import React from "react";
-import "./style.css"
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
+import emailjs from '@emailjs/browser';
+import Alert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
 
 
 const ContactForm = () => {
-    // const [status, setStatus] = useState("Submit");
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     setStatus("Sending...");
-    //     const {name, email, message} = e.target.elements;
-    //     let details = {
-    //         name: name.value,
-    //         email: email.value,
-    //         message: message.value,
-    //     };
-    //     let response = await fetch("http://localhost:5000/contact", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json;charset=utf-8",
-    //         },
-    //         body: JSON.stringify(details),
-    //     });
-    //     setStatus("Submit");
-    //     let result = await response.json();
-    //     alert(result.status);
-    // };
+    const [open, setOpen] = React.useState(false);
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: {errors}
+    } = useForm();
+
+    const onSubmit = async (data) => {
+        const {name, email, subject, message} = data;
+        try {
+            const templateParams = {
+                name,
+                email,
+                subject,
+                message
+            };
+            await emailjs.send(
+                process.env.REACT_APP_SERVICE_ID,
+                process.env.REACT_APP_TEMPLATE_ID,
+                templateParams,
+                process.env.REACT_APP_USER_ID
+            );
+            reset();
+             setOpen(true);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+
+
+
     return (
-        <Paper className="contact"  align="center"
+        <Paper className="contact" align="center"
                occupation={3}
-               style={{background:"#ffffff1a",color:"white"}} >
-            <h2>For any question contact me on: </h2>
-            <h3>alexdruzina[at]gmail[dot]com</h3>
+               style={{background: "rgba(255,255,255,0.55)"}}
+        >
+            <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
+                <div style={{display: "grid",gridGap: "10px"}}>
+                <TextField
+                    type='text'
+                    name='name'
+                    {...register('name', {
+                        required: {value: true, message: 'Please enter your name'},
+                        maxLength: {
+                            value: 30,
+                            message: 'Please use 30 characters or less'
+                        }
+                    })}
+                    placeholder='Name'
+                />
+                {errors.name && <span>{errors.name.message}</span>}
+
+                <TextField
+                    type='email'
+                    name='email'
+                    {...register('email', {
+                        required: true,
+                        pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                    })}
+                    placeholder='Email address'
+                />
+                {errors.email && (
+                    <span>Please enter a valid email address</span>
+                )}
+                <TextField
+                    type='text'
+                    name='subject'
+                    {...register('subject', {
+                        required: {value: true, message: 'Please enter a subject'},
+                        maxLength: {
+                            value: 75,
+                            message: 'Subject cannot exceed 75 characters'
+                        }
+                    })}
+                    placeholder='Subject'
+                />
+                {errors.subject && (
+                    <span>{errors.subject.message}</span>
+                )}
+                <TextField
+                    multiline
+                    rows={3}
+                    name='message'
+                    {...register('message', {
+                        required: true
+                    })}
+                    placeholder='Message'
+                />
+                {errors.message && <span>Please enter a message</span>}
+                </div>
+                <Button variant="outlined" type='submit' style={{margin: "10px"}}>
+                    Submit
+                </Button>
+            </form>
+            <Snackbar open={open} autoHideDuration={6000} >
+                <Alert  severity="success" sx={{width: '100%'}}>
+                    Message sent successfully!
+                </Alert>
+            </Snackbar>
         </Paper>
 
-
-
-
-        // <form onSubmit={handleSubmit}>
-        //     <Paper className="contact"
-        //
-        //         elevation={3}
-        //         >
-        //         <TextField fullWidth
-        //                    label="Name"
-        //                    id="name"
-        //                    type="text"
-        //                    required/>
-        //         <TextField
-        //             fullWidth
-        //             label="Email"
-        //             id="email"
-        //             type="email"
-        //             required/>
-        //         <TextField
-        //                    fullWidth
-        //                    label="Message"
-        //                    id="message"
-        //                    multiline
-        //                    minRows={4}
-        //                    required/>
-        //
-        //         <Button
-        //             size="small"
-        //             variant="outlined"
-        //             type="submit"
-        //             style={{padding: "1%", marginTop: "2%", marginLeft: "43%", alignText: "center"}}
-        //         >
-        //             {status}
-        //         </Button>
-        //     </Paper>
-        // </form>
     );
 };
 
