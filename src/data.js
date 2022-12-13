4923b5f3fdc3792a38f77eb438e9b2a0277cfb2c
AlexDruzina <alexdruzina@gmail.com>
2022-12-13 11:31:38 +0200

    update ProjectInfo

--- src/data.js
+++ src/data.js
@@ -29,7 +29,6 @@ const technologies = [
     },
 
 
-
 ];
 
 const projects = [
@@ -41,7 +40,9 @@ const projects = [
             "● Backend in Python (Flask, SQLAlchemy), Frontend in Javascript (React)," +
             "Firebase for SSO and persisting key-value (Json) data, MySQL for" +
             "persisting internal table-based data.\n" +
-            "● Best project award in 2021.",
+            "● Best project award in 2021.\n" +
+            "● GitHub Repo: https://github.com/saashd/CPMS \n" +
+            "● Live: https://cpms.iem.technion.ac.il",
         image: require('./icons/icon_cpms.jpg'),
         link: "https://cpms.iem.technion.ac.il/",
     },
@@ -51,7 +52,8 @@ const projects = [
         description:
             "A mobile application for health traking. Used Arduino to track steps during a walk or a run together with route, and to track vital signs.\n" +
             "● Technologies: Java, Google APIs, Firebase.\n" +
-            "● Demonstration video can be found in GitHub repo.\n",
+            "● Demonstration video can be found in GitHub repo.\n" +
+            "● GitHub Repo: https://github.com/saashd/IOT-Project",
         image: "",
         link: "https://github.com/saashd/IOT-Project",
     },
@@ -61,14 +63,17 @@ const projects = [
         description:
             "Web App for creating custom memes.\n" +
             "● Technologies:  JS,React,Material UI and imgflip \n" +
-            "● Live Demo: stellular-jelly-eaf7b4.netlify.app ",
+            "● GitHub Repo: https://github.com/saashd/meme_generator \n" +
+            "● Live Demo: https://stellular-jelly-eaf7b4.netlify.app",
         image: require('./icons/meme.jpg'),
-        link: "https://github.com/saashd/meme_generator",
+        link: "https://stellular-jelly-eaf7b4.netlify.app",
     },
     {
         title: "ML Projects",
         subtitle: "",
-        description: "Project following “ControlVAE: Tuning, Analytical Properties, and Performance Analysis” paper \n",
+        description:
+            "Project following “ControlVAE: Tuning, Analytical Properties, and Performance Analysis” paper \n" +
+            "● GitHub Repo: https://github.com/saashd/controlVAE",
         image: "",
         link: "https://github.com/saashd/controlVAE",
     },
--- src/layout/ProjectInfo.js
+++ src/layout/ProjectInfo.js
@@ -2,21 +2,29 @@ import DialogTitle from "@mui/material/DialogTitle";
 import React from "react";
 import {Dialog, DialogContent,} from "@mui/material";
 
-
 function ProjectInfo({project, handleClose, open}) {
+
+    function replaceURLWithHTMLLinks(text) {
+        //eslint-disable-next-line
+        let exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        if (text) {
+            return text.replace(exp, "<a id='link' target='_blank' href='$1'>click</a>")
+
+        }
+
+    }
 
     return (
         <Dialog
             BackdropProps={{style: {backgroundColor: 'rgba(255,255,255,0.11)'}}}
             onClose={handleClose} open={open}>
             <DialogTitle align="center"
-            style={{background:"#000000c9"}}>
+                         style={{background: "#000000c9"}}>
                 {project.image ?
                     <img className="img" alt=""
                          src={project.image}
                     />
                     : <></>}
-
                 <a
                     className="zoom"
                     style={{
@@ -37,12 +45,11 @@ function ProjectInfo({project, handleClose, open}) {
                 <h3>
                     {project.subtitle}
                 </h3>
-                <p style={{whiteSpace: "pre-wrap"}}>{project.description}</p>
+                <div style={{whiteSpace: "pre-wrap"}}
+                   dangerouslySetInnerHTML={{__html: replaceURLWithHTMLLinks(project.description)}}/>
             </DialogContent>
 
         </Dialog>
     );
-
 }
-
 export default ProjectInfo;
